72d96a64b7eaf5b47b6fade86a7db09bf0e4fba8
Masr3 <manuelsantanar03@gmail.com>
2023-02-03 10:43:37 -0400

    Agregando el proyecto a Github

---
+++ src/main/java/rd/masr3/obspringejercicio_3/ObSpringEjercicio3Application.java
@@ -0,0 +1,13 @@
+package rd.masr3.obspringejercicio_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSpringEjercicio3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObSpringEjercicio3Application.class, args);
+    }
+
+}
